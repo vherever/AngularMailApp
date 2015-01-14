3f9f968aa43f5972add18a01d8d6e07c6f0d67cc
Vladimir Vherever <vherever@gmail.com>
2015-01-14 16:27:15 +0200

    add directive, returning data info

--- app.js
+++ app.js
@@ -16,27 +16,62 @@ angular.module('MailApp', ['ngRoute'])
 			});
 	})
 
-	.controller('InboxCtrl', function InboxCtrl(InboxFactory) {
+	.controller('InboxCtrl', function InboxCtrl() {
 		'use strict';
-		InboxFactory.getMessages().success(function(data) {
-			debugger;
-		});
+	
+		
 		this.title = "My Inbox"
 	})
 	.controller('EmailCtrl', function EmailCtrl() {
 		this.title = "This is Email partial"
 	})
 
-	.factory('InboxFactory', function InboxFactory($http) {
+	.factory('InboxFactory', function InboxFactory($q, $http) {
 		'use strict';
 		var exports = {};
 		exports.messages = [];
 		
 		exports.getMessages = function() {
-			return $http.get('emails.json');
-		}
+			var deferred = $q.defer();
+			return $http.get('emails.json')
+				.success(function(data) {
+					exports.messages = data;
+					deferred.resolve(data);
+				})
+				.error(function(data) {
+					deferred.reject(data);
+				});
+				return deferred.promise;
+		};
 
 		return exports;
+	})
+
+
+//Directive allows us to use <inbox><inbox/> tags
+	.directive('inbox', function InboxDirective() {
+		'use strict';
+
+		return {
+			restrict: 'EA',
+			replace: true,
+			scope: true,
+			templateUrl: 'partials/inbox.tmpl.html',
+			controllerAs: 'inbox',
+
+			controller: function(InboxFactory) {
+				this.messages = [];
+
+				InboxFactory.getMessages()
+					.then(angular.bind(this, function() {
+						this.messages = InboxFactory.messages;
+					}) );
+			},
+
+			link: function(scope, element, attrs, ctrl) {
+
+			}
+		}
 	});
 
 	
